d078762a350e77375a16d489d9aa613b39cd2b56
NotSilverrr <tronsin7@gmail.com>
2025-01-07 22:35:16 +0100

    make title red

--- app1/resources/views/welcome.blade.php
+++ app1/resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                         </g>
                     </svg>
                 </div>
-                <h2 style="color: white">SERV 1</h2>
+                <h2 style="color: red">SERV 1</h2>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">

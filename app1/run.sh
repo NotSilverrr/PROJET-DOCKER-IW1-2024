3e762e54249c10ee82b7f9f69b480f2687d24313
NotSilverrr <tronsin7@gmail.com>
2025-01-20 15:21:56 +0100

    fix volumes and dockerfile update

--- app1/run.sh
+++ app1/run.sh
@@ -1,2 +1,10 @@
+chmod -R 775 /var/www/html/app1/storage /var/www/html/app1/bootstrap/cache
+chmod -R 775 /var/www
+composer update && composer install --no-dev --optimize-autoloader
+php artisan key:generate
+
+npm install
+npm run build
+
 php artisan migrate:fresh --seed
 php-fpm
--- app2/run.sh
+++ app2/run.sh
@@ -1,2 +1,10 @@
+chmod -R 775 /var/www/html/app2/storage /var/www/html/app2/bootstrap/cache
+chmod -R 775 /var/www
+composer update && composer install --no-dev --optimize-autoloader
+php artisan key:generate
+
+npm install
+npm run build
+
 php artisan migrate:fresh --seed
 php-fpm
